210246087f917f3bd3dc3538f90c1f99c5c200c7
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2022-07-22 16:43:22 -0300

    v2

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,5 @@
 let visorInput = document.getElementById("visor");
+let select = document.getElementById("select");
 let tecla = document.getElementById("tecla");
 let n1 = "";
 let n2 = "";
@@ -6,24 +7,43 @@ let operador = "";
 
 function limpar() {
   visorInput.value = "";
+  select.innerText = "";
 }
 
 function adicionar(numero) {
-      visorInput.value += numero;  
+  visorInput.value += numero;
 }
 
 function clickOperador(n) {
+  select.innerText = visorInput.value;
   operador = n;
   n1 = visorInput.value;
   visorInput.value = "";
+  
+  if (operador == "+") {    
+    select.innerText +="+";        
+  }
+
+  if (operador == "-") {
+    select.innerText += "-";
+  }
+
+  if (operador == "x") {
+    select.innerText += "x";
+  }
+
+  if (operador == "/") {
+    select.innerText += "/";
+  }
 }
 
 function resultado() {
   n2 = visorInput.value;
+  select.innerText += visorInput.value;
   let resultado;
   if (operador == "+") {
     resultado = Number(n1) + Number(n2);
-  }
+  } 
 
   if (operador == "-") {
     resultado = Number(n1) - Number(n2);
